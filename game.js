ef7b789d9c865d840b95dede9738f1238ce59124
Madeleine & Martin <pair+madeleine-rashbrooke.martybutts@devbootcamp.com>
2016-07-21 11:51:24 +1200

    double strike working

--- game.js
+++ game.js
@@ -2,6 +2,16 @@ module.exports = {
   scoreFrame: scoreFrame
 }
 
-function scoreFrame (frame) {
-  return frame[0] + frame[1]
+function scoreFrame (frame, nextFrame, nextNextFrame) {
+  var score = frame[0] + frame[1]
+  if (frame[0] === 10) {
+    score += nextFrame[0] + nextFrame[1]
+    if (nextFrame[0] === 10) {
+      score += nextNextFrame[0]
+    }
+  }
+  else if (score === 10) {
+    score += nextFrame[0]
+  }
+  return score
 }
--- tests/index.js
+++ tests/index.js
@@ -14,3 +14,28 @@ test('scores a normal frame', function(t) {
   t.equals(score, 5)
   t.end()
 })
+
+test('scores a spare frame', function(t) {
+  var frame = [4, 6]
+  var nextFrame = [4,4]
+  var score = game.scoreFrame(frame, nextFrame)
+  t.equals(score, 14)
+  t.end()
+})
+
+test('scores a single strike frame', function(t) {
+  var frame = [10,0]
+  var nextFrame = [4,4]
+  var score = game.scoreFrame(frame, nextFrame)
+  t.equals(score, 18)
+  t.end()
+})
+
+test('scores a double strike frame', function(t) {
+  var frame = [10,0]
+  var nextFrame = [10,0]
+  var nextNextFrame = [5,3]
+  var score = game.scoreFrame(frame, nextFrame, nextNextFrame)
+  t.equals(score, 25)
+  t.end()
+})
