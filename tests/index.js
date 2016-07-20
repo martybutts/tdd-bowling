206ef005c50d33aed7b02418e328f541602cd0fd
Madeleine & Martin <pair+madeleine-rashbrooke.martybutts@devbootcamp.com>
2016-07-21 11:26:10 +1200

    scoring normal frames

--- tests/index.js
+++ tests/index.js
@@ -7,3 +7,10 @@ test('scores a gutterball frame', function(t) {
   t.equals(score, 0)
   t.end()
 })
+
+test('scores a normal frame', function(t) {
+  var frame = [2, 3]
+  var score = game.scoreFrame(frame)
+  t.equals(score, 5)
+  t.end()
+})
