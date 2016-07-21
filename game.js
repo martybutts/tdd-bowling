41ae388f22309aa1b480d01701e865b5d71a82a4
Madeleine & Martin <pair+madeleine-rashbrooke.martybutts@devbootcamp.com>
2016-07-21 15:02:55 +1200

    refactored with isSpare isStrike scoreSpare scoreStrike functions

--- game.js
+++ game.js
@@ -3,15 +3,45 @@ module.exports = {
 }
 
 function scoreFrame (frame, nextFrame, nextNextFrame) {
-  var score = frame[0] + frame[1]
-  if (frame[0] === 10) {
-    score += nextFrame[0] + nextFrame[1]
-    if (nextFrame[0] === 10) {
-      score += nextNextFrame[0]
-    }
+  if (isStrike(frame)) {
+    return scoreStrike(frame, nextFrame, nextNextFrame)
   }
-  else if (score === 10) {
-    score += nextFrame[0]
+  else if (isSpare(frame)) {
+    return scoreSpare(frame, nextFrame)
+  }
+  return frame[0] + frame[1]
+}
+
+function scoreStrike(frame, nextFrame, nextNextFrame){
+  var score = frame[0] + frame[1] + nextFrame[0] + nextFrame[1]
+  if (nextFrame[0] === 10) {
+    score += nextNextFrame[0]
   }
   return score
 }
+
+function scoreSpare(frame, nextFrame){
+  return frame[0] + frame[1] + nextFrame[0] //returns frame score (number)
+}
+
+function isStrike(frame) {
+  return frame[0] === 10
+}
+
+function isSpare(frame) {
+  return (frame[0] + frame[1]) === 10   //true or false
+}
+
+// function scoreFrame (frame, nextFrame, nextNextFrame) {
+//   var score = frame[0] + frame[1]
+//   if (frame[0] === 10) {
+//     score += nextFrame[0] + nextFrame[1]
+//     if (nextFrame[0] === 10) {
+//       score += nextNextFrame[0]
+//     }
+//   }
+//   else if (score === 10) {
+//     score += nextFrame[0]
+//   }
+//   return score
+// }
